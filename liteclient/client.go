39afb803d7ec18e3ef6ceb4e7c6521efe2c528dc
Xa No <xano3425@gmail.com>
2017-09-15 21:16:23 +0200

    Add Addresses method to liteclient

--- liteclient/client.go
+++ liteclient/client.go
@@ -1,5 +1,9 @@
 package liteclient
 
+import (
+	"github.com/skycoin/skycoin/src/cipher"
+)
+
 type Wallet struct {
 	Seed string
 	Addresses int
@@ -11,3 +15,17 @@ type Address struct {
 	Coins uint64
 	Hours uint64
 }
+
+func Addresses(seed string, amount int) ([]Address, error) {
+	_, secretKeys := cipher.GenerateDeterministicKeyPairsSeed([]byte(seed), amount)
+	addresses := make([]Address, amount)
+	for i, sec := range secretKeys {
+		pub := cipher.PubKeyFromSecKey(sec)
+		address := Address{
+			cipher.AddressFromPubKey(pub).String(),
+			sec.Hex(),
+			0,
+			0,
+		}
+		addresses[i] = address
+	}
