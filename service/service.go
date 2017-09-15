44d36913bf385eca362dbdb5dcffb9141d5100b1
Xa No <xano3425@gmail.com>
2017-09-15 21:11:52 +0200

    Add global address and result methods

--- service/service.go
+++ service/service.go
@@ -1,8 +1,24 @@
 package service
 
+const NodeAddress = "121.41.103.148:8080"
+
 type Result struct {
 	Success          *bool   `protobuf:"varint,1,req,name=success" json:"success,omitempty"`
 	Errcode          *int32  `protobuf:"varint,2,opt,name=errcode" json:"errcode,omitempty"`
 	Reason           *string `protobuf:"bytes,3,opt,name=reason" json:"reason,omitempty"`
 	XXX_unrecognized []byte  `json:"-"`
+}
+
+func (m *Result) GetSuccess() bool {
+	if m != nil && m.Success != nil {
+		return *m.Success
+	}
+	return false
+}
+
+func (m *Result) GetReason() string {
+	if m != nil && m.Reason != nil {
+		return *m.Reason
+	}
+	return ""
 }
